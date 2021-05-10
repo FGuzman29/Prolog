6d4c0fc9fe66b2a81410b37f98a1d52d1fe91c47
lendry02 <marrero0812@gmail.com>
2021-05-10 11:57:04 -0400

    Ejercicio 2.1 y 2.2

--- Compendio1.pl
+++ Compendio1.pl
@@ -10,3 +10,19 @@ progenitor(elena,fernando).
 progenitor(elena,silvia).
 progenitor(eva,emilio).
 progenitor(david,emilio).
+
+legusta(maria,pasta) .
+legusta(juan,carne) .
+legusta(juan,vino).
+legusta(jose,pasta).
+legusta(jose,carne).
+legusta(jose,cerveza).
+
+% Ejercicio 2.1
+legusta(juan,pasta) .
+
+% Ejercicio 2.2
+legusta(juan,maria) .
+legusta(juan,jose) .
+legusta(juan,juan) .
+
