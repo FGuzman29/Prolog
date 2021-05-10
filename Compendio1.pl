f7d6a5faa740c53c0741338d11f51bdd308dd0fe
Felix Guzman <49173087+FGuzman29@users.noreply.github.com>
2021-05-10 11:07:39 -0400

    Adición de los hechos

--- Compendio1.pl
+++ Compendio1.pl
@@ -1,1 +1,12 @@
 %Compendio #1 Grupo 6
+
+progenitor(antonio,carlos).
+progenitor(antonio,eva).
+progenitor(maria,carlos).
+progenitor(maria,eva).
+progenitor(carlos,fernando).
+progenitor(carlos,silvia).
+progenitor(elena,fernando).
+progenitor(elena,silvia).
+progenitor(eva,emilio).
+progenitor(david,emilio).
