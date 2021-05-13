26f749d323988a0a90a023a6841fb7102f420868
Dreamy <20170502@ce.pucmm.edu.do>
2021-05-13 10:16:45 -0400

    Update

--- Compendio1.pl
+++ Compendio1.pl
@@ -1,5 +1,16 @@
 %Compendio #1 Grupo 6
+progenitor(antonio,carlos).
+progenitor(antonio,eva).
+progenitor(maria,carlos).
+progenitor(maria,eva).
+progenitor(carlos,fernando).
+progenitor(carlos,silvia).
+progenitor(elena,fernando).
+progenitor(elena,silvia).
+progenitor(eva,emilio).
+progenitor(david,emilio).
 
-pareja(X,Y):-.
-suegro(Suegro,Yerno):-.
-cunado(X,Y):-.
+hermano(X,Y):- progenitor(Z,X), progenitor(Z,Y).
+pareja(X,Y):- progenitor(X,Z), progenitor(Y,Z).
+%suegro(Suegro,Yerno):-.
+%cunado(X,Y):-.
